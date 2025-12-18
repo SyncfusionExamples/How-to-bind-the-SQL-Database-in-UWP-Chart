807fe60f5ee27e3126726cae808c62bc4522a0ed
subash_s <subash.s@syncfusion.com>
2025-12-18 13:28:25 +0530

    code update: resolve the database connection issue in uwp

--- SQLwithChartUWP/MainPage.xaml.cs
+++ SQLwithChartUWP/MainPage.xaml.cs
@@ -1,22 +1,4 @@
-﻿using Microsoft.Data.Sqlite;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
+﻿using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,47 +9,9 @@ namespace SQLLiteChartBinding
     /// </summary>
     public sealed partial class MainPage : Page
     {
-
         public MainPage()
         {
-            this.InitializeComponent();
-
+            InitializeComponent();
         }       
-    }
-
-    public class ViewModel
-    {
-        public ViewModel()
-        {
-            try
-            {
-                SqlConnection thisConnection = new SqlConnection(ConnectionString);
-                thisConnection.Open();
-                string Get_Data = "SELECT * FROM ChartData";
-                SqlCommand cmd = thisConnection.CreateCommand();
-                cmd.CommandText = Get_Data;
-                SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                sda.Fill(ds);
-                var table = ds.Tables[0];
-                this.DataTable = table;
-            }
-            catch
-            {
-                return;
-            }
-        }
-
-        public object DataTable { get; set; }
-
-        public static string ConnectionString
-        {
-            get
-            {
-                string currentDir = System.Environment.CurrentDirectory;
-                currentDir = currentDir.Substring(0, currentDir.Length - 10) + "\\LocalDataBase";
-                return @"Data Source=Data Source=ata Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\RachelA\source\repos\SQLLiteChartBinding\LocalDataBase\SeriesItemsSource.mdf;Integrated Security=True;Connect Timeout=30";
-            }
-        }
     }
 }
